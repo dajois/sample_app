cf19c4c759efef2204de9a1b13184c23a727f16b
dajois14 <dajois14@gmail.com>
2019-08-06 18:10:49 +0900

    保存とリダイレクトを行う、userのcreateアクション

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class UsersSighupTest < ActionDispatch::IntegrationTest
+class UsersSignupTest < ActionDispatch::IntegrationTest
 
   test "invalid signup information" do
     get signup_path
@@ -8,8 +8,8 @@ class UsersSighupTest < ActionDispatch::IntegrationTest
       post users_path, params: { user: { name: "", email: "user@invalid", password: "foo", password_confirmation: "bar" } }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
   end
-  # test "the truth" do
-  #   assert true
-  # end
+
 end
