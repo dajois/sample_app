679c4dcf1cc1edcc708415ffb1f51a12f9386ee1
dajois14 <dajois14@gmail.com>
2019-08-06 15:48:24 +0900

    fix typo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,11 +13,11 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
 
     private
 
       def user_params
         params.require(:user).permit(:name, :email, :password, :passowrd_confirmation)
       end
-  end
 end
