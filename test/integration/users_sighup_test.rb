534ba19f56daa1a92082b56e24ac098942670dd0
dajois14 <dajois14@gmail.com>
2019-08-06 15:58:56 +0900

    無効なユーザー登録に対するテスト

---
+++ test/integration/users_sighup_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class UsersSighupTest < ActionDispatch::IntegrationTest
+
+  test "invalid sighup information" do
+    get sighup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name: "", email: "user@invalid", password: "foo", password_confirmation: "bar" } }
+    end
+    assert_template 'users/new'
+  end
+  # test "the truth" do
+  #   assert true
+  # end
+end
