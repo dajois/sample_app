58fec1e2744c630249f873c7e2b6d4cb5b78d88c
dajois14 <dajois14@gmail.com>
2019-08-08 09:44:49 +0900

    ユーザーをデータベースから見つけて検証する

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #
+    else
+      render 'new'
+    end
   end
 
   def destory
