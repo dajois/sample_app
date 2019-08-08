fcdfc9db03e2681f96cb6e6b9bef87235e50d0a2
dajois14 <dajois14@gmail.com>
2019-08-08 14:34:21 +0900

    ログイン失敗時の正しい処理

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       #
     else
+      flash.now[:danger] = 'Invalid email/passowrd combination'
       render 'new'
     end
   end
