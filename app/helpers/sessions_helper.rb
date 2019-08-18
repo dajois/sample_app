8d7b26dcc715874b8ea7de957064d059a5e9bffd
dajois14 <dajois14@gmail.com>
2019-08-18 21:29:43 +0900

    永続的セッションのcurrent_userを更新する

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,10 +7,18 @@ module SessionsHelper
     user.remember
     cookies.parmanent.signed[:user_id] = user.id
     cookies.parmanent[:remember_token] = user.remember_token
+  end
+
   #現在ログイン中のユーザを返す(いる場合)
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @currnet_user = user
+      end
     end
   end
 
