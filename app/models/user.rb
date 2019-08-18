b3ee6f48c91d8655c59e07df3d5027d122075b3e
dajois14 <dajois14@gmail.com>
2019-08-18 20:06:28 +0900

    authenticated?をUserモデルに追加する

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,4 +26,9 @@ class User < ApplicationRecord
     self.remember_token = User.new_token        #記憶トークンをremember_tokenに代入
     update_attribute(:remember_digest, User.digest(remember_token)) #DBのremember_token属性値をBcryptに渡してハッシュ化して更新
   end
+
+  #渡されたトークンがダイジェストと一致したらtrueを返す
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 end
