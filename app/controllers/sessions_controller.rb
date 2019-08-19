5960e806ba6059f03195dac28fc629f3614c4b59
dajois14 <dajois14@gmail.com>
2019-08-19 16:12:12 +0900

    createアクション内でインスタンス変数を使うためのテンプレート

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    ?user = User.find_by(email: params[:session][:email].downcase)
-    if ?user && ?user.authenticate(params[:session][:password])
-      log_in ?user
-      params[:session][:remember_me] == '1' ? remember(?user):forget(?user)
-      redirect_to ?user
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user):forget(@user)
+      redirect_to @user
     else
       flash.now[:danger] = 'Invalid email/passowrd combination'
       render 'new'
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out if logged_in? #ログイン中のときのみログアウトする
+    log_out if logged_in #ログイン中のときのみログアウトする
     redirect_to root_url
   end
 end
