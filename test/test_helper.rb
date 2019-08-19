9f448735f317f8de004d39c0148b6d15cc33cbb8
dajois14 <dajois14@gmail.com>
2019-08-19 15:51:44 +0900

    log_in_asヘルパーを追加する

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,10 +9,19 @@ class ActiveSupport::TestCase
   fixtures :all
   include ApplicationHelper
 
-  # テストユーザーがログイン中の場合にtrueを返す
+  #テストユーザがログイン中の場合にtrueを返す
   def is_logged_in?
     !session[:user_id].nil?
   end
-
+  #テストユーザとしてログインする
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
   # Add more helper methods to be used by all tests here...
+end
+
+class ActionDispatch::IntegrationTest
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email, password, remember_me: rememberme }}
+  end
 end
