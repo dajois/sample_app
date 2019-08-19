f01f1e5d1031f98dc4286ecf301583a5d1baff0b
dajois14 <dajois14@gmail.com>
2019-08-19 16:00:05 +0900

    fix typo

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -22,6 +22,6 @@ end
 
 class ActionDispatch::IntegrationTest
   def log_in_as(user, password: 'password', remember_me: '1')
-    post login_path, params: { session: { email: user.email, password, remember_me: rememberme }}
+    post login_path, params: { session: { email: user.email, password: password, remember_me: remember_me }}
   end
 end
